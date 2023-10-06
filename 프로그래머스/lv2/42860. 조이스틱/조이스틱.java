41b9f6fa79f195ced0c026f5d797e9303c97276f
SHplusR <111962797+SHplusR@users.noreply.github.com>
2023-10-07 01:12:50 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,33 @@
+// 1. s,e는 같은 0번쨰에 시작한다. 
+// 2. 시작 - 끝 거리를 왼방향/ 오방향 중 더 짧은걸로 더한다. 그리고 해당 인덱스 값을 더한다. 
+// 3. s를 뒤로 보낸다. 
+// 4. 2~3 과정을 s가 끝까지 갈때까지 버팀.
+
+class Solution {
+    
+    static int solution(String name) {
+        int answer = 0;
+        int[] nameList = new int[name.length()];
+        int length = name.length();
+        int movement = length-1;
+        for(int i=0;i<length;i++){
+                if((int)name.charAt(i)>78){
+                    nameList[i] = (Math.abs((int)name.charAt(i)-90)+1);
+                }
+                else{
+                    nameList[i] = (int)name.charAt(i)-65;
+                }
+                answer +=  nameList[i];
+            }
+
+            for(int i = 0; i < length; i++){
+                int idx = i+1;
+                // 연속되는 A 갯수 확인
+                while(idx < length && nameList[idx] == 0){
+                    idx++;
+                }
+                movement = Math.min(movement, i+(length-idx)+Math.min(i,length-idx));
+            }
+            return answer+movement;
+        }
+}
